3e3e581c4112e42739b6b1b0c87439ce90b11eea
Isaac Seessel <iseessel@oberlin.edu>
2020-11-24 15:45:45 -0500

    Add stock picking optimization algorithm framework

---
+++ stock_pickers/stock_picker.py
@@ -0,0 +1,9 @@
+class StockPicker:
+  def __init__(self, **kwargs):
+    self.predictions = kwargs['predictions']
+
+  """
+    Generates a portfolio based on the predictions.
+  """
+  def generate_portfolio(self):
+    raise('#generate_portfolio method not yet implemented!')
